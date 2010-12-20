d2eb51b0e48f9560a10d1fbca84bd9e09e2d56be
Lubomir Rintel <lkundrak@v3.sk>
2010-12-20 16:36:13 +0100

    A major tidy up
    Add comments, perldoc, option parsing.

--- npm2rpm.pl
+++ npm2rpm.pl
@@ -1,42 +1,96 @@
+#!/bin/env perl
+
+=head1 NAME
+
+npm2rpm - Generate RPM packages SPEC from node.js NPM metadata
+
+=head1 SYNOPSIS
+
+npm2rpm
+[--specdir <specdir>]
+[--sourcedir <sourcedir>]
+[--user <user>]
+[--email <email>]
+[--date <date>]
+[[--package] <package>]
+[-h|--help] [-H|--man]
+
+=head1 DESCRIPTION
+
+The utility is designed to auto-generate skeleton of rpm package
+from NPM metadata. The resulting package is intended to comply with
+Fedora packaging guidelines.
+
+npm2rpm will download the package metadata, determine the URL of
+distribution tarball, download it and write a spec file called
+nodejs-<package>.spec into your SPEC file directory.
+
+=cut
+
+use Getopt::Long;
 use JSON::XS;
 use LWP::Simple;
+use Pod::Usage;
 
 use strict;
 use warnings;
 
+# Defaults
 my $sourcedir = `rpm --eval '%{_sourcedir}'`;
 chomp $sourcedir;
 my $specdir = `rpm --eval '%{_specdir}'`;
 chomp $specdir;
+my $date = `date +'%a %b %d %Y'`;
+chomp $date;
+my $user = $ENV{USER};
+my $email = [split (/:/, `getent passwd $user`)]->[4];
+my $package;
 
-my $package = shift
-	or die 'Missing package argument';
+# Parse command line options
+GetOptions (
+	'h|help'	=> sub { pod2usage({-verbose => 1}); exit },
+	'H|man'		=> sub { pod2usage({-verbose => 2}); exit },
+	"specdir=s"	=> \$specdir,
+	"sourcedir=s"	=> \$sourcedir,
+	"user=s"	=> \$user,
+	"email=s"	=> \$email,
+	"date=s"	=> \$date,
+	"package=s"	=> \$package,
+) or die "Run $0 -h or $0 -H for details on usage";
 
+$package = shift @ARGV if @ARGV and not $package;
+die 'Missing package argument' unless $package;
+die "Extra arguments: @ARGV" if @ARGV;
+
+# Get details from central NPM repository
 # FIXME: Server returns... not quite JSON.
 my $metadata_json = `npm view $package`;
 $metadata_json =~ s/\b([^"':\s]+)(:\s+)/"$1"$2/gm;
 $metadata_json =~ s/'/"/gm;
 my $metadata = decode_json ($metadata_json);
+die 'Could not  read and parse package metadata'
+	unless $metadata;
 
-my $date = `date +'%a %b %d %Y'`;
-chomp $date;
-my $user = $ENV{USER};
-my $email = [split (/:/, `getent passwd $user`)]->[4];
-
+# Create the SPEC file
 open (SPEC, ">$specdir/nodejs-$metadata->{name}.spec")
-	or die $!;
+	or die "Could not write into SPEC file: $!";
 
+# Pull the tarball
 my $tarball = $sourcedir.[$metadata->{dist}{tarball} =~ /(\/[^\/]+)$/]->[0];
 mirror ($metadata->{dist}{tarball}, $tarball) or die $!;
+my $source = $metadata->{dist}{tarball};
 
+# Find out the top-level directory from tarball
+# The upstreams often use very weird ones
 my $dir = [`tar tzf $tarball` =~ /([^\/]+)/]->[0];
+
+# Use macros wherever possible
+$source =~ s/$metadata->{version}/%{version}/g;
+$source =~ s/$metadata->{name}/%{npmname}/g;
 $dir =~ s/$metadata->{version}/%{version}/g;
 $dir =~ s/$metadata->{name}/%{npmname}/g;
 
-my $source = $metadata->{dist}{tarball};
-$source =~ s/$metadata->{version}/%{version}/g;
-$source =~ s/$metadata->{name}/%{npmname}/g;
-
+# Build and runtime dependencies
 my @dependencies;
 foreach my $name (keys %{$metadata->{dependencies}}) {
 	my $version = $metadata->{dependencies}{$name};
@@ -47,6 +101,7 @@ foreach my $name (keys %{$metadata->{dependencies}}) {
 }
 @dependencies = ('', @dependencies, '') if @dependencies;
 
+# Keep track of runnable scripts (that go into /usr/bin)
 my @bininst;
 my @binfiles;
 for my $bin (map { $_, "$_@%{version}" } keys %{$metadata->{bin}}) {
@@ -56,6 +111,7 @@ for my $bin (map { $_, "$_@%{version}" } keys %{$metadata->{bin}}) {
 @bininst = ('', 'mkdir -p $RPM_BUILD_ROOT%{_bindir}', @bininst) if @bininst;
 @binfiles = (@binfiles, '') if @binfiles;
 
+# Fill in the template
 print SPEC <<EOF;
 %global npmname $metadata->{name}
 
@@ -131,3 +187,81 @@ print SPEC <<EOF;
 EOF
 
 close SPEC;
+
+=head1 OPTIONS
+
+=over
+
+=item B<--specdir> B<< <specdir> >>
+
+Where to write the resulting SPEC file to.
+Defaults to B<%_specdir> RPM macro, which is usually
+~/rpmbuild/SPECS (or /usr/src/redhat/SPECS on ancient
+RPM versions).
+
+=item B<--sourcedir> B<< <sourcedir> >>
+
+Where to download the upstream distribution tarball to.
+Defaults to B<%_sourcedir> RPM macro, which is usually
+~/rpmbuild/SOURCES (or /usr/src/redhat/SOURCES on ancient
+RPM versions).
+
+=item B<--date> B<< <date> >>
+
+Date to use for initial changelog entry (e.g. Mon Dec 20 2010).
+Defaults to output of C<date +'%a %b %d %Y'>.
+
+=item B<--user> B<< <user> >>
+
+Name (in "Firstname Lastname") format to use for initial
+changelog entry. Defaults to your account's real name.
+
+=item B<--email> B<< <email> >>
+
+E-mail address to use in initial changelog entry.
+Defaults to your login.
+
+=item [B<--package>] B<< <package> >>
+
+The NPM package name to act upon. Mandatory.
+
+=item B<-h>, B<--help>
+
+Prints and error message and exits
+
+=item B<-H>, B<--manual>
+
+Show the complete manual page and exit.
+
+=back
+
+=head1 BUGS
+
+The resulting package is ugly as hell -- due the way NPM currently
+works the whole npm tree has to be copied for unprivileged build,
+which is inefficient.
+
+NPM packages sometimes lack pieces of metadata this tool uses;
+you may see "uninitialized" warnings from perl, as well as empty
+or "FIXME" tags in the resulting SPEC file.
+
+While for simple packages and good configuration the package can
+be used as it is, the weirder package is the higher are chances
+you'll have to hand-edit it.
+
+=head1 EXAMPLES
+
+=over 4
+
+=item C<npm2rpm jsdom>
+
+Well, nothing particularly sophisticated.
+
+=back
+
+=head1 SEE ALSO
+
+L<cpanspec>, L<https://fedoraproject.org/wiki/User:Lkundrak/NodeJS>
+
+
+=cut
